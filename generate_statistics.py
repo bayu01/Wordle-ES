ecc06eaa5a52cdbd031ba1c76c7971b0bb0b8fcd
Pedro Jacques <jacques.pedro@gmail.com>
2022-02-03 22:31:53 -0600

    print letter frequency

--- generate_statistics.py
+++ generate_statistics.py
@@ -1,19 +1,19 @@
 import numpy as np
-from scipy import stats as stats
 
 
 def project_list(file_name):
-    li = []
     with open(file_name, 'r', encoding="utf8") as f_in:
         li = [x.strip() for x in f_in]
     print(len(li))
 
-    word_array = np.array(li, dtype=np.unicode_ )
+    word_array = np.array(li, dtype=np.unicode_)
     char_2d_array = word_array.view('U1').reshape((word_array.size, -1))
 
-    print(char_2d_array)
-
-    print(stats.mode(char_2d_array))
+    # print(char_2d_array)
+    unique, counts = np.unique(char_2d_array[:, 0], return_counts=True)
+    assert len(unique) == len(counts)
+    letter_freq = dict(zip(unique, counts))
+    print(''.join([f'"{k}": {v}\n' for k, v in letter_freq.items()]))
 
 
 if __name__ == '__main__':
