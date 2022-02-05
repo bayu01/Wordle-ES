f4c4f0b36099ee0c7376620498ad3507b2fd431d
Pedro Jacques <jacques.pedro@gmail.com>
2022-02-05 13:24:24 -0600

    get letter weight

--- generate_statistics.py
+++ generate_statistics.py
@@ -10,10 +10,16 @@ def project_list(file_name):
     char_2d_array = word_array.view('U1').reshape((word_array.size, -1))
 
     # print(char_2d_array)
-    unique, counts = np.unique(char_2d_array[:, 0], return_counts=True)
+    unique, counts = np.unique(char_2d_array, return_counts=True)
     assert len(unique) == len(counts)
     letter_freq = dict(zip(unique, counts))
     print(''.join([f'"{k}": {v}\n' for k, v in letter_freq.items()]))
+
+    total_letters = char_2d_array.size
+    print(f'total letters {total_letters}')
+
+    letters_wt_values = dict(map(lambda kv: (kv[0], kv[1]/total_letters), letter_freq.items()))
+    print(''.join([f'"{k}": {v}\n' for k, v in letters_wt_values.items()]))
 
 
 if __name__ == '__main__':
